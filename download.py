163e93486ac914912b7e7128eade9f021ec869be
Uyi <91153600+uyi1001@users.noreply.github.com>
2021-11-26 11:24:43 -0500

    Add files via upload

---
+++ download.py
@@ -0,0 +1,5 @@
+import requests
+temp = requests.get("https://localprod.pandateacher.com/python-manuscript/crawler-html/exercise/HTTP%E5%93%8D%E5%BA%94%E7%8A%B6%E6%80%81%E7%A0%81.md")
+target = temp.text
+with open ('download.txt','w') as f:
+    f.write(target)
